209daf50632c1f448ee5a5029b686c16c5614fe6
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-03-04 17:07:48 -0500

    Finish Weather App

--- src/main/java/com/tts/WeatherApp/repository/ZipCodeRepository.java
+++ src/main/java/com/tts/WeatherApp/repository/ZipCodeRepository.java
@@ -2,15 +2,11 @@ package com.tts.WeatherApp.repository;
 
 import com.tts.WeatherApp.model.ZipCode;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ZipCodeRepository extends CrudRepository<ZipCode,Long> {
-
-    List<ZipCode> findAll();
-    //List<ZipCode> findByZipCode(String zipCode);
-    ZipCode deleteById(long id);
-    ZipCode findById(long id);
+public interface ZipCodeRepository extends PagingAndSortingRepository<ZipCode, Long> {
 }
